d24274527a21af525612a30461971d5389e3e30d
Shaik <672260@cognizant.com>
2020-08-06 23:32:53 +0530

    First Commit

---
+++ src/test/java/com/example/SpringBootBuildingBlocks/SpringBootBuildingBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootBuildingBlocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildingBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
